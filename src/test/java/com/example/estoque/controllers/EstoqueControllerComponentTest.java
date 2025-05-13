ab1a0d38b7c02dc712c44c882d9875e4bfecae7e
lucasnp <lucaspinto86@gmail.com>
2025-05-12 21:23:33 -0300

    Novos testes

--- src/test/java/com/example/estoque/controllers/EstoqueControllerComponentTest.java
+++ src/test/java/com/example/estoque/controllers/EstoqueControllerComponentTest.java
@@ -1,7 +1,10 @@
 package com.example.estoque.controllers;
 
 import com.example.estoque.controller.EstoqueController;
+import com.example.estoque.domain.Produto;
+import com.example.estoque.service.ProdutoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +14,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +29,8 @@ public class EstoqueControllerComponentTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ProdutoService produtoService;
 
     @Test
     public void givenEstoqueIsNull_whenGetEstoque_thenStatus200() throws Exception {
@@ -38,5 +45,20 @@ public class EstoqueControllerComponentTest {
 
     }
 
+    @Test
+    public void givenProductHasNoName_whenGetEstoque_thenStatus400() throws Exception {
+        var result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/estoque")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        if (result.getStatus() != 200) {
+            throw new Exception();
+        }
+
+    }
 
 }
+
+
+
